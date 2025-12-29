f00259af5158f6608968377755be899959dd9b4e
Selix2002 <smunmuno@umag.cl>
2025-12-29 18:32:14 +0100

    Cambios al gitignore

---
+++ infra/envs/Dev-dynamodb/outputs.tf
@@ -0,0 +1,7 @@
+output "ddb_table_name" {
+  value = module.ddb.table_name
+}
+
+output "ddb_table_arn" {
+  value = module.ddb.table_arn
+}
---
+++ infra/envs/Dev-dynamodb/variables.tf
@@ -0,0 +1,12 @@
+variable "project_prefix" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
